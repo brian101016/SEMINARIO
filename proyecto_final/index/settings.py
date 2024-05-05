fd390d5c9f8189ba8c8e72a108335cb0a3f370b0
Compa-Pulparindo <rivera.lopez.p.prog@gmail.com>
2024-05-04 17:52:58 -0700

    Creación de app Matrimonio

--- proyecto_final/index/settings.py
+++ proyecto_final/index/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "webapp",
+    "matrimonios",
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,14 @@ WSGI_APPLICATION = "index.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   "default": {
+      "ENGINE": "django.db.backends.postgresql_psycopg2",
+      "NAME": "proyectoFinal",
+      "USER": "postgres",
+      "PASSWORD": "admin",
+      "HOST": "localhost",
+      "PORT": "5432",
+   }
 }
 
 
