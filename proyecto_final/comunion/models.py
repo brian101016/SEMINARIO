1f34fcafe30c494564f8c2082c37c9f8840548e0
Orlando <orlandoqe23@gmail.com>
2024-05-06 13:38:46 -0700

    modulo-comunion

---
+++ proyecto_final/comunion/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Comunion(models.Model):
+    nombre = models.CharField(max_length=255)
+    sexo = models.BooleanField(default=False)
+    padre = models.CharField(max_length=255)
+    madre = models.CharField(max_length=255)
+    padrino_madrina = models.CharField(max_length=255)
+    ciudad_bautizo = models.CharField(max_length=255, default="Hermosillo, Sonora")
+    parroquia_bautizo = models.CharField(max_length=255)
+    fecha_bautizo = models.DateField()
+
+    def __str__(self):
+        return self.nombre
